eaf3b1d212d2af24e741360ed4c02433969e041c
Ruslan Khlebnikov <61401591+liltrill@users.noreply.github.com>
2020-03-30 11:59:07 +0400

    Add files via upload

---
+++ precache-manifest.a140c9ff38d918f930af2e067d1c977e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99tjRqBNW8zk7HEoCDU4",
+    "url": "./static/css/main.660cc2cf.chunk.css"
+  },
+  {
+    "revision": "99tjRqBNW8zk7HEoCDU4",
+    "url": "./static/js/main.0036c28d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1a8671c34064cd54e728",
+    "url": "./static/css/2.90d8283f.chunk.css"
+  },
+  {
+    "revision": "1a8671c34064cd54e728",
+    "url": "./static/js/2.4c7ce8f1.chunk.js"
+  },
+  {
+    "revision": "765511cb0554c2fc3b23813154eb0c53",
+    "url": "./index.html"
+  }
+];
